d08b58ddd40ba68f3d17b022e4246ded0aab6c4b
coolequal09 <devesh2cse@gmail.com>
2024-10-27 10:55:40 +0530

    read to use

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,15 @@ import viteLogo from '/vite.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
 
   return (
     <>
       <div className='justify-center'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => (count)*(count) + 1)}>count is {count}
-        </button>
-        <button onClick={() => setCount1((count1) => (count1)*(-count1) - 1)}>
-          count is {count1}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+        
       <p className='justify-center text-red-400 text-center'>
         Click on the Vite and React logos to learn more
       </p>
+      </div>
     </>
   )
 }
