bc9dc9a08574a38ba23f93c5f176a85f5f300a9c
coolequal09 <devesh2cse@gmail.com>
2024-10-29 23:08:07 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-
   return (
     <>
-      <div className='justify-center'>
-        
-      <p className='justify-center text-red-400 text-center'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="justify-center">
+        <p className="justify-center text-red-400 text-center">
+          Click on the Vite and React logos to learn more hii
+        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
